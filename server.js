58e78c9e71e10a0defcec5d34cf45a13a52b2229
Alvin Lin <alvin.lin@stuypulse.com>
2015-08-29 14:14:47 -0400

    base files

---
+++ server.js
@@ -0,0 +1,33 @@
+var PORT_NUMBER = process.env.PORT || 5000;
+
+// Dependencies
+var async = require('async');
+var bodyParser = require('body-parser');
+var express = require('express');
+var swig = require('swig');
+var https = require('https');
+
+var app = express();
+
+app.engine('html', swig.renderFile);
+
+app.set('port', PORT_NUMBER);
+app.set('view engine', 'html');
+
+app.use('/bower_components',
+        express.static(__dirname + '/bower_components'));
+app.use('/static',
+        express.static(__dirname + '/static'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.get('/', function(request, response) {
+  response.render('index.html', {
+    percentage: 0.1
+  });
+});
+
+var server = require('http').Server(app);
+server.listen(PORT_NUMBER, function() {
+  console.log('Listening to port ' + PORT_NUMBER);
+});
