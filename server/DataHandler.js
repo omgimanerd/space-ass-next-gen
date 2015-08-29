05daa1eb51d0a6209b402186dd4e9cd6636472a8
Alvin Lin <alvin.lin@stuypulse.com>
2015-08-29 17:06:12 -0400

    Created DataHandler clas

---
+++ server/DataHandler.js
@@ -0,0 +1,40 @@
+/**
+ * This file contains the class necessary to interpret latitude-longitude data
+ * given back by the Google API.
+ * @author Alvin Lin (alvin.lin@stuypulse.com)
+ */
+
+// Dependencies
+var fs = require('fs');
+
+function DataHandler(meteoriteData) {
+  this.meteoriteData = meteoriteData;
+
+  this.setup();
+}
+
+DataHandler.prototype.setup = function() {
+  var context = this;
+  fs.readFile('data/meteorites.json', function(err, data) {
+    if (err) {
+      throw err;
+    }
+    context.meteoriteData = JSON.parse(data);
+  });
+};
+
+DataHandler.prototype.getNearbyMeteors = function(latitude, longitude,
+                                                  threshold) {
+  var nearbyMeteors = {};
+  for (var meteor in this.meteoriteData) {
+    if (Math.abs(this.meteoriteData[meteor].latitude - latitude) <
+            threshold &&
+        Math.abs(this.meteoriteData[meteor].longitude - longitude) <
+            threshold) {
+      nearbyMeteors[meteor] = this.meteoriteData[meteor];
+    }
+  }
+  return nearbyMeteors;
+};
+
+module.exports = DataHandler;
